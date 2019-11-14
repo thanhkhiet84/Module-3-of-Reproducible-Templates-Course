4a10f587b03b80a26b90e88dca9634056f4f003e
thanhkhiet84 <56431799+thanhkhiet84@users.noreply.github.com>
2019-11-14 19:08:12 +0700

    new R script

---
+++ script1.R
@@ -0,0 +1,11 @@
+library(fivethirtyeight)
+library(tidyverse)
+
+data("steak_survey",package="fivethirtyeight")
+view(steak_survey)
+
+sdat<-na.omit(steak_survey)%>%filter(region=="Mountain")
+
+ggplot(sdat,aes(x=steak_prep,fill=female))+
+  geom_bar(position = "dodge")+
+  ggtitle(paste0("Steak Preparation Preference by Gender for", "Mountain", "Region"))
